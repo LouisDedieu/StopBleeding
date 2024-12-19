0280437787069bccc73537b3ce87b0bd909dfc13
LouisDedieu <115251020+LouisDedieu@users.noreply.github.com>
2024-12-19 11:01:28 +0100

    init project

---
+++ StopBleeding-frontend/constants/Colors.ts
@@ -0,0 +1,21 @@
+const tintColorLight = '#1f77bb';
+const tintColorDark = '#fff';
+
+export default {
+  light: {
+    text: '#000',
+    background: '#fff',
+    tint: tintColorLight,
+    tabIconDefault: '#353535',
+    tabIconSelected: tintColorLight,
+    tabBackgroundSelected: '#D2E2EF'
+  },
+  dark: {
+    text: '#fff',
+    background: '#000',
+    tint: tintColorDark,
+    tabIconDefault: '#353535',
+    tabIconSelected: tintColorDark,
+    tabBackgroundSelected: '#D2E2EF'
+  },
+};
