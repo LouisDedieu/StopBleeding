5ccd0956589165f7dd2b48d98e56bfa6cad3f0ac
LouisDedieu <115251020+LouisDedieu@users.noreply.github.com>
2024-12-20 16:00:44 +0100

    add logo

--- StopBleeding-frontend/app/(tabs)/_layout.tsx
+++ StopBleeding-frontend/app/(tabs)/_layout.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Link, Tabs } from 'expo-router';
-import {Pressable} from 'react-native';
+import {Image, Pressable} from 'react-native';
 import Icon from "react-native-remix-icon";
 
 import Colors from '@/constants/Colors';
 import { useColorScheme } from '@/components/useColorScheme';
 import { useClientOnlyValue } from '@/components/useClientOnlyValue';
+import Logo from '@/assets/images/LogoStopBleeding.png';
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
@@ -26,6 +27,8 @@ export default function TabLayout() {
                 tabBarInactiveTintColor: Colors[colorScheme ?? 'light'].tabIconDefault,
                 tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tabIconDefault,
                 headerShown: useClientOnlyValue(false, true),
+                headerShadowVisible: false,
+                headerTitle: '',
                 tabBarShowLabel: false,
             }}
             >
@@ -33,38 +36,48 @@ export default function TabLayout() {
                 name="index"
                 options={{
                     tabBarIcon: ({color}) => <Icon name={"home-3-fill"} size={35} color={color} style={{marginTop: 8}}/>,
-                    headerRight: () => (
-                        <Link href="/modal" asChild>
-                            <Pressable>
-                                {({pressed}) => (
-                                    <FontAwesome
-                                        name="info-circle"
-                                        size={25}
-                                        color={Colors[colorScheme ?? 'light'].text}
-                                        style={{marginRight: 15, opacity: pressed ? 0.5 : 1}}
-                                    />
-                                )}
-                            </Pressable>
-                        </Link>
-                    ),
+                    headerLeft: () => (
+                        <Image
+                            source={Logo}
+                            style={{ marginLeft: 5, marginTop: 5, height: 60, width: 60}}
+                        />
+                    )
                 }}
             />
             <Tabs.Screen
                 name="two"
                 options={{
                     tabBarIcon: ({color}) => <Icon name={"dossier-line"} size={35} color={color} style={{marginTop: 8}}/>,
+                    headerLeft: () => (
+                        <Image
+                            source={Logo}
+                            style={{ marginLeft: 5, marginTop: 5, height: 60, width: 60}}
+                        />
+                    )
                 }}
             />
             <Tabs.Screen
                 name="qrcode"
                 options={{
                     tabBarIcon: ({ color }) => <Icon name={"qr-code-line"} size={35} color={color} style={{marginTop: 8}}/>,
+                    headerLeft: () => (
+                        <Image
+                            source={Logo}
+                            style={{ marginLeft: 5, marginTop: 5, height: 60, width: 60}}
+                        />
+                    )
                 }}
             />
             <Tabs.Screen
                 name="settings"
                 options={{
                     tabBarIcon: ({ color }) => <Icon name={'settings-5-line'} size={35} color={color} style={{marginTop: 8}}/>,
+                    headerLeft: () => (
+                        <Image
+                            source={Logo}
+                            style={{ marginLeft: 5, marginTop: 5, height: 60, width: 60}}
+                        />
+                    )
                 }}
             />
         </Tabs>
