bd8ea5e5e49a0247dabc615a18538d2ae5e4657f
LouisDedieu <louis.victordedieu@gmail.com>
2024-12-23 18:50:06 +0100

    update qr code page

--- StopBleeding-frontend/app/(tabs)/qrcode.tsx
+++ StopBleeding-frontend/app/(tabs)/qrcode.tsx
@@ -2,14 +2,22 @@ import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import CustomButton from "@/components/CustomButton";
+import Colors from "@/constants/Colors";
+import {useColorScheme} from "@/components/useColorScheme";
 
 export default function TabTwoScreen() {
+  const colorScheme = useColorScheme();
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
-    </View>
+      <View style={styles.container} lightColor={Colors[colorScheme ?? 'light'].tintBackground} darkColor={Colors[colorScheme ?? 'light'].tintBackground}>
+        <View style={styles.subcontainer}>
+          <View style={styles.qrholder} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+        </View>
+
+        <CustomButton onPress={() => {}} text={"SCANNER"} />
+
+      </View>
   );
 }
 
@@ -19,13 +27,23 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  subcontainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+    flexGrow: 0.9,
+    width: '90%',
+    borderRadius: 10,
+    marginTop: 15,
+  },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
   },
-  separator: {
+  qrholder: {
     marginVertical: 30,
-    height: 1,
-    width: '80%',
+    width: '95%',
+    aspectRatio: 1,
+    borderRadius: 10,
   },
 });
